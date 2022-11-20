14f84c235eafe22e83160cf45727a2dab49913ca
IvanUt4 <59344006+Exmokou@users.noreply.github.com>
2022-11-20 14:34:21 -0700

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -4,5 +4,6 @@ lista2 = ["Mokou","Yukari","Katakuri"]
 
 letra  = str(input("Ingrese la letra que quiere analizar  "))
 letra =letra.lower()
+print(sq)
 print(list(filter(lambda num: num%2 == 0,lista)))
 print(list(filter(lambda st: (st[0].lower() == letra),lista2)))
